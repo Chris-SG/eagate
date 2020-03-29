8025406d254db9a68887814d6a1d066ad359f9fa
Chris-SG <christopher.shandgost@myob.com>
2020-03-30 00:05:45 +1100

    Add recent score and workout tests

--- ddr/ddr_users_test.go
+++ ddr/ddr_users_test.go
@@ -269,7 +269,104 @@ func TestSongStatisticsForClient(t *testing.T) {
 }
 
 func TestRecentScoresFromDocument(t *testing.T) {
+	// Setup test
+	const testFile = "./test_data/player/recent_scores.html"
 
+	// Setup expected results
+	timeFormat := "2006-01-02 15:04:05"
+	timeLocation, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		glog.Warningln("failed to load timezone location Asia/Tokyo")
+		return
+	}
+
+	expectedRecentScores := make([]ddr_models.Score, 0)
+
+	date, _ := time.ParseInLocation(timeFormat, "2020-03-18 18:52:17", timeLocation)
+	expectedRecentScores = append(expectedRecentScores, ddr_models.Score{
+		Score: 818290,
+		ClearStatus: false,
+		TimePlayed: date,
+		SongId: "b1do8OI6qDDlQO0PI16868ql6bdbI886",
+		Mode: "SINGLE",
+		Difficulty: "EXPERT",
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-18 18:49:09", timeLocation)
+	expectedRecentScores = append(expectedRecentScores, ddr_models.Score{
+		Score: 987390,
+		ClearStatus: true,
+		TimePlayed: date,
+		SongId: "08PO96OlIoQqPdq91Q1Qqlo8lPidbPP8",
+		Mode: "SINGLE",
+		Difficulty: "EXPERT",
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-18 18:46:39", timeLocation)
+	expectedRecentScores = append(expectedRecentScores, ddr_models.Score{
+		Score: 989630,
+		ClearStatus: true,
+		TimePlayed: date,
+		SongId: "1OlD9Iqb9Oqol09dDi6iiQ9Iod1oP0il",
+		Mode: "SINGLE",
+		Difficulty: "EXPERT",
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-18 18:43:30", timeLocation)
+	expectedRecentScores = append(expectedRecentScores, ddr_models.Score{
+		Score: 946550,
+		ClearStatus: true,
+		TimePlayed: date,
+		SongId: "8QbqP80q9PI8bbi0qOoiibOQD08OPdli",
+		Mode: "SINGLE",
+		Difficulty: "DIFFICULT",
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-18 18:39:36", timeLocation)
+	expectedRecentScores = append(expectedRecentScores, ddr_models.Score{
+		Score: 835080,
+		ClearStatus: true,
+		TimePlayed: date,
+		SongId: "bilO9D91P1lo81oIDP6qOoqdDdQdoDlP",
+		Mode: "SINGLE",
+		Difficulty: "EXPERT",
+		PlayerCode: 12345678,
+	})
+
+	// Run Test
+	document, err := documentFromFile(testFile)
+	if err != nil {
+		t.Fatalf("could not load %s: %s", testFile, err.Error())
+	}
+
+	recentScores, err := recentScoresFromDocument(document, 12345678)
+
+	if err != nil {
+		t.Errorf("failed to load chart stats from document: %s", err.Error())
+	}
+
+	for _, rs := range recentScores {
+		match := false
+		for _, ers := range expectedRecentScores {
+			if rs.PlayerCode == ers.PlayerCode &&
+				rs.Score == ers.Score &&
+				rs.ClearStatus == ers.ClearStatus &&
+				rs.TimePlayed.String() == ers.TimePlayed.String() &&
+				rs.SongId == ers.SongId &&
+				rs.Mode == ers.Mode &&
+				rs.Difficulty == ers.Difficulty {
+				match = true
+				break
+			}
+		}
+		if !match {
+			t.Errorf("could not find a match for score: %+#v", rs)
+		}
+	}
 }
 
 func TestRecentScoresForClient(t *testing.T) {
@@ -277,7 +374,86 @@ func TestRecentScoresForClient(t *testing.T) {
 }
 
 func TestWorkoutDataFromDocument(t *testing.T) {
+	// Setup test
+	const testFile = "./test_data/player/workout.html"
 
+	// Setup expected results
+	timeFormat := "2006-01-02"
+	timeLocation, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		glog.Warningln("failed to load timezone location Asia/Tokyo")
+		return
+	}
+
+	expectedWorkoutData := make([]ddr_models.WorkoutData, 0)
+
+	date, _ := time.ParseInLocation(timeFormat, "2020-03-18", timeLocation)
+	expectedWorkoutData = append(expectedWorkoutData, ddr_models.WorkoutData{
+		Date:       date,
+		PlayCount:  11,
+		Kcal:       343.35,
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-16", timeLocation)
+	expectedWorkoutData = append(expectedWorkoutData, ddr_models.WorkoutData{
+		Date:       date,
+		PlayCount:  17,
+		Kcal:       613.958,
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-11", timeLocation)
+	expectedWorkoutData = append(expectedWorkoutData, ddr_models.WorkoutData{
+		Date:       date,
+		PlayCount:  12,
+		Kcal:       412.469,
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-06", timeLocation)
+	expectedWorkoutData = append(expectedWorkoutData, ddr_models.WorkoutData{
+		Date:       date,
+		PlayCount:  7,
+		Kcal:       207.611,
+		PlayerCode: 12345678,
+	})
+
+	date, _ = time.ParseInLocation(timeFormat, "2020-03-05", timeLocation)
+	expectedWorkoutData = append(expectedWorkoutData, ddr_models.WorkoutData{
+		Date:       date,
+		PlayCount:  17,
+		Kcal:       538.68,
+		PlayerCode: 12345678,
+	})
+
+	// Run Test
+	document, err := documentFromFile(testFile)
+	if err != nil {
+		t.Fatalf("could not load %s: %s", testFile, err.Error())
+	}
+
+	workoutData, err := workoutDataFromDocument(document, 12345678)
+
+	if err != nil {
+		t.Errorf("failed to load chart stats from document: %s", err.Error())
+	}
+
+	for _, wd := range workoutData {
+		match := false
+		for _, ewd := range expectedWorkoutData {
+			if wd.PlayerCode == ewd.PlayerCode &&
+				wd.PlayCount == ewd.PlayCount &&
+				wd.Kcal == ewd.Kcal &&
+				wd.Date.String() == ewd.Date.String() {
+				match = true
+				break
+			}
+		}
+		if !match {
+			t.Errorf("could not find a match for workout data: %+#v", wd)
+		}
+	}
 }
 
 func TestWorkoutDataForClient(t *testing.T) {
